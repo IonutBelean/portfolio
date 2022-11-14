2f4ce33919af43b3761cf7a40e0d5d64cc4bb03b
unknown <you@example.comionutbelean4@gmail.com>
2022-11-14 20:47:05 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,31 +4,33 @@ import { Nav, Navbar, Container } from "react-bootstrap";
 const Header = () => {
   return (
     <Navbar expand="lg" className={`${HeaderCSS.navbar}`}>
-      <Container className={`${HeaderCSS.container} d-flex justify-between`}>
-        <div>
-          <Navbar.Brand href="#home" className={`${HeaderCSS.link}`}>
-            Ioan Belean
-          </Navbar.Brand>
-        </div>
-        <div>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav>
-              <Nav.Link href="#" className={`${HeaderCSS.link}`}>
-                Home
-              </Nav.Link>
-              <Nav.Link href="#about" className={`${HeaderCSS.link}`}>
-                About
-              </Nav.Link>
-              <Nav.Link href="#skills" className={`${HeaderCSS.link}`}>
-                Skills
-              </Nav.Link>
-              <Nav.Link href="#projects" className={`${HeaderCSS.link}`}>
-                Projects
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
+      <Container className={`${HeaderCSS.container}  `}>
+        <Navbar.Brand href="#home" className={`${HeaderCSS.link} `}>
+          Ioan Belean
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className={`${HeaderCSS.show} `}
+        />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          // className={`${HeaderCSS.collapse}`}
+        >
+          <Nav className={`${HeaderCSS.collapse}`}>
+            <Nav.Link href="#" className={`${HeaderCSS.link}`}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="#about" className={`${HeaderCSS.link}`}>
+              About
+            </Nav.Link>
+            <Nav.Link href="#skills" className={`${HeaderCSS.link}`}>
+              Skills
+            </Nav.Link>
+            <Nav.Link href="#projects" className={`${HeaderCSS.link}`}>
+              Projects
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
