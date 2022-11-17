53ee41b8a16166824e7e93f6f938425d97709a81
unknown <you@example.comionutbelean4@gmail.com>
2022-11-17 19:13:39 +0200

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,62 @@
 import { Col, Container, Row } from "react-bootstrap";
 import AboutCSS from "./About.module.css";
+import { motion } from "framer-motion";
+
+const imageAnimation = {
+  offscreen: { y: 100, opacity: 0 },
+  onscreen: { y: 0, opacity: 1 },
+  transition: { type: "spring", bounce: 0.5, duration: 5 },
+};
+const borderAnimation = {
+  offscreen: { opacity: 0 },
+  onscreen: { opacity: 1 },
+  transition: { type: "spring", bounce: 0.5, duration: 0 },
+};
+const textAnimation = {
+  offscreen: { x: 100, opacity: 0 },
+  onscreen: { x: 0, opacity: 1 },
+  transition: { type: "spring", bounce: 0.5, duration: 0 },
+};
+
+const resumeAnimation = {
+  offscreen: { x: 100, opacity: 0 },
+  onscreen: { x: 0, opacity: 1 },
+  transition: { type: "spring", bounce: 0.5, duration: 0 },
+};
 
 const About = () => {
   return (
     <Container className={`${AboutCSS.container}`}>
       <Row className="g-3">
         <Col lg={6} md={6} sm={12}>
-          <div className={`${AboutCSS.img_box}`}>
-            <div className={`${AboutCSS.border}`}></div>
-            <img
+          <motion.div
+            className={`${AboutCSS.img_box}`}
+            initial={"offscreen"}
+            whileInView={"onscreen"}
+            viewport={{ once: false, amount: 0.3 }}
+            transition={{ staggerChildren: 0.2 }}
+          >
+            <motion.div
+              className={`${AboutCSS.border}`}
+              variants={borderAnimation}
+            ></motion.div>
+            <motion.img
               src="./images/img.JPG"
               alt="This is me."
               className={`${AboutCSS.img}`}
+              variants={imageAnimation}
             />
-          </div>
+          </motion.div>
         </Col>
         <Col lg={6} md={6} sm={12}>
-          <div className={`${AboutCSS.text_box}`}>
-            <div>
+          <motion.div
+            className={`${AboutCSS.text_box}`}
+            initial={"offscreen"}
+            whileInView={"onscreen"}
+            viewport={{ once: false, amount: 0.3 }}
+            transition={{ staggerChildren: 0.3 }}
+          >
+            <motion.div variants={textAnimation}>
               Hi, I'm a <span>Junior Front-end Developer</span>, with a passion
               for making <span>beautiful</span>, <span>responsive</span>{" "}
               websites. I like the <span>creativity</span> and{" "}
@@ -27,8 +66,8 @@ const About = () => {
               <span>full time development</span> role with mentorship, room to
               grow and <span>positive</span> people. I am a fan of movies, video
               games, books and music.
-            </div>
-            <div>
+            </motion.div>
+            <motion.div variants={resumeAnimation}>
               <a
                 href="/resume/CV.pdf"
                 download
@@ -42,8 +81,8 @@ const About = () => {
                 />
                 Resume
               </a>
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </Col>
       </Row>
     </Container>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,19 +1,30 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import SkillsCSS from "./Skills.module.css";
 import { motion } from "framer-motion";
 
 const leftImageAnimation = {
   offscreen: { x: -100, opacity: 0 },
   onscreen: { x: 0, opacity: 1 },
-  // rotate: [0, 45, 0],
   transition: { type: "spring", bounce: 0.4, duration: 1 },
 };
-const leftImageAnimation2 = {
-  offscreen: { x: -100, opacity: 0 },
+const rightImageAnimation = {
+  offscreen: { x: 100, opacity: 0 },
   onscreen: { x: 0, opacity: 1 },
-  // rotate: [0, 45, 0],
   transition: { type: "spring", bounce: 0.4, duration: 1 },
 };
+
+// const imagesAndText = [
+//   { image: "/images/html.PNG", alt: "HTML icon", text: "HTML" },
+//   { image: "/images/css.PNG", alt: "CSS icon", text: "CSS" },
+//   {
+//     image: "/images/javascript.PNG",
+//     alt: "JavaScript icon",
+//     text: "JavaScript",
+//   },
+//   { image: "/images/react.PNG", alt: "React icon", text: "React" },
+//   { image: "/images/bootstrap.PNG", alt: "Bootstrap icon", text: "Bootstrap" },
+//   { image: "/images/github.PNG", alt: "GitHub icon", text: "GitHub" },
+// ];
 
 const Skills = () => {
   return (
@@ -23,32 +34,35 @@ const Skills = () => {
         initial={"offscreen"}
         whileInView={"onscreen"}
         viewport={{ once: false, amount: 0.2 }}
-        transition={{ staggerChildren: 0.5 }}
+        transition={{ staggerChildren: 0.2 }}
       >
         <motion.li variants={leftImageAnimation}>
           <img src="/images/html.PNG" alt="HTML icon language" />
           <p>HTML</p>
         </motion.li>
-        <motion.li variants={leftImageAnimation2}>
+        <motion.li variants={leftImageAnimation}>
           <img src="/images/css.PNG" alt="CSS icon language" />
           <p>CSS</p>
         </motion.li>
-        <li>
+        <motion.li variants={leftImageAnimation}>
           <img src="/images/javascript.PNG" alt="JavaScript icon language" />
           <p>JavaScript</p>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={rightImageAnimation}>
           <img src="/images/react.PNG" alt="React icon framework" />
           <p>React</p>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={rightImageAnimation}>
           <img src="/images/bootstrap.PNG" alt="BootsTrap icon framework" />
           <p>Bootstrap</p>
-        </li>
-        <li className={`${SkillsCSS.github}`}>
+        </motion.li>
+        <motion.li
+          className={`${SkillsCSS.github}`}
+          variants={rightImageAnimation}
+        >
           <img src="/images/github.PNG" alt="GitHub icon tool" />
           <p>GitHub</p>
-        </li>
+        </motion.li>
       </motion.ul>
     </Container>
   );
