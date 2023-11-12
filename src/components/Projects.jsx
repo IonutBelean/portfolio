c43e3204ff7202cc7be5ad4f95e93d2d8988bb37
IonutBelean <ionutbelean4@gmail.com>
2023-11-12 15:54:20 +0200

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -100,6 +100,44 @@ const Projects = () => {
             </div>
           </Card>
         </motion.div>
+        <motion.div variants={projectAnimation}>
+          <Card style={{ width: "18rem" }} className={`${ProjectsCSS.card}`}>
+            <div className={`${ProjectsCSS.face} ${ProjectsCSS.face1}`}>
+              <Card.Img
+                variant="top"
+                src="/images/underfive.PNG"
+                className={`${ProjectsCSS.img} `}
+              />
+            </div>
+            <div className={`${ProjectsCSS.face} ${ProjectsCSS.face2}`}>
+              <Card.Title className={`${ProjectsCSS.title}`}>
+                E-Commerce
+              </Card.Title>
+              <Card.Text className={`${ProjectsCSS.text}`}>
+                {" "}
+                <img src="/images/react.PNG" alt="React icon framework" />
+              </Card.Text>
+              <div
+                className={`${ProjectsCSS.btns} d-flex justify-content-around`}
+              >
+                <Card.Link
+                  href="https://underfive.netlify.app/"
+                  target="_blank"
+                  className={`${ProjectsCSS.btn}`}
+                >
+                  Live
+                </Card.Link>
+                <Card.Link
+                  href="https://github.com/IonutBelean/e-commerce"
+                  target="_blank"
+                  className={`${ProjectsCSS.btn}`}
+                >
+                  Code
+                </Card.Link>
+              </div>
+            </div>
+          </Card>
+        </motion.div>
       </motion.div>
     </Container>
   );
